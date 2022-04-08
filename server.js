044b082d94fdc5c2fcd8a4c9b939015a2d6031b7
Alabama Whiskey <amwtablet@gmail.com>
2022-04-07 20:08:37 -0400

    added ejs views and page routing for registration and main app

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+app.set("view engine", "ejs")
+
 
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.render('login');
 });
+
+app.get('/users/register', (req, res) => {
+    req.render("register");
+})
+
+app.get('/users/dashboard', (req, res) => {
+    req.render("dashboard", { user: "andy" });
+})
 
 app.listen(PORT, () => {
     console.log(`Server is Running on PORT ${ PORT }`)
