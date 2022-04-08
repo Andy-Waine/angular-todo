d64081f8deda947bc5e55c18daf03156ccd71594
Alabama Whiskey <amwtablet@gmail.com>
2022-04-07 22:30:52 -0400

    added form validation for registration

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const { pool } = require("./dbConfig");
 
 const PORT = process.env.PORT || 4000;
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 
 app.get('/', (req, res) => {
@@ -15,6 +17,30 @@ app.get('/users/register', (req, res) => {
 
 app.get('/users/dashboard', (req, res) => {
     res.render("dashboard", { user: "andy" });
+})
+
+app.post('/users/register', (req,res)=>{
+    let {name, email, password, password2 } = req.body;
+
+    console.log({name, email, password, password2});
+
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ message: "Please Enter All Fields" });
+    }
+
+    if(password.length < 6) {
+        errors.push({ message: "Password Must Be at least 6 Characters" });
+    }
+
+    if(password != password2) {
+        errors.push({ message: "Passwords Do Not Match, Please Try Again" });
+    }
+
+    if (errors.length > 0) {
+        res.render('register', { errors });
+    }
 })
 
 app.listen(PORT, () => {
